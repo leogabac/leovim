052f9d57de7eb8fdca82d1252a5f1bda9ddf0d54
leogabac <leogabac@gmail.com>
2024-12-30 22:52:56 -0600

    treesitter disable auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return { -- Highlight, edit, and navigate code
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'python', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
