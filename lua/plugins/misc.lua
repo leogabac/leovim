75c6afae1fd599d317c7ee3394b582d26359f3fb
leogabac <leogabac@gmail.com>
2025-02-20 10:23:16 -0600

    image folke false inline

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -18,9 +18,7 @@ return {
     ---@type snacks.Config
     opts = {
       image = {
-        -- your image configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        inline = false,
       },
     },
   },
