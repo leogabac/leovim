9f314962dde6e703e467172cfa36b6cce99a034d
Leonardo Alanis <leogabac@gmail.com>
2025-04-25 00:04:36 -0600

    show untracked files, and some other git integrations

--- lua/plugins/files/nvim-tree.lua
+++ lua/plugins/files/nvim-tree.lua
@@ -1,42 +1,50 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  version = "*", -- Use the latest version
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Optional, for file icons
+  version = "*",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    -- Disable netrw (native file explorer) to avoid conflicts
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
     require("nvim-tree").setup({
-      -- Basic options
       view = {
-        width = 35, -- Width of the tree (columns)
-        side = "left", -- Position: "left" or "right"
+        width = 35,
+        side = "left",
       },
-      -- File/folder icons
       renderer = {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = "▶", -- Closed folder arrow
-              arrow_open = "▼", -- Open folder arrow
+              arrow_closed = "▶",
+              arrow_open = "▼",
             },
           },
         },
+        -- Highlight Git icons
+        highlight_git = true,
+        -- Show Git icons next to file names
+        indent_markers = {
+          enable = true,
+        },
       },
-      -- File ignoring
       filters = {
-        dotfiles = false, -- Show hidden files (true to hide)
+        dotfiles = false,
       },
-      -- Key mappings
+      -- Git integration
+      git = {
+        enable = true,       -- Enable Git integration
+        ignore = false,      -- Show ignored files (default: false)
+        timeout = 500,       -- Timeout for Git commands (ms)
+        show_on_dirs = true, -- Show Git status on directories
+        show_on_open_dirs = true,
+      },
       actions = {
         open_file = {
-          quit_on_open = true, -- Close tree when opening a file
+          quit_on_open = true,
         },
       },
     })
 
-    -- Key mapping to toggle nvim-tree
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
   end,
 }
